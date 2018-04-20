9554e0f5e0e05edc4b1341b4fcf79d6bcec9f5d6
William WATKINS <wwatkins@e2r8p18.42.fr>
2018-04-20 17:45:22 +0200

    Keanu Reeves from the matrix

--- include/Matrix.hpp
+++ include/Matrix.hpp
@@ -6,23 +6,30 @@
 
 #include "Exception.hpp"
 
+#define RAD2DEG (180.0 / M_PI)
+#define DEG2RAD (M_PI / 180.0)
+
 /* H being the height (or rows), W being the width (or cols) */
 template<typename T, size_t H, size_t W>
 class Mat2d {
 
 public:
     Mat2d( void ) : w(W), h(H), size(W * H) {
-        for (size_t i = 0; i < size; ++i)
-            data[i] = 0;
+        if (h == w)
+            this->identity();
+        else {
+            for (size_t i = 0; i < size; ++i)
+                data[i] = 0;
+        }
     }
     Mat2d( const std::array<T, W * H>& value ) : w(W), h(H), size(W * H) {
         for (size_t i = 0; i < size; ++i)
             data[i] = value[i];
     }
-    Mat2d( Mat2d const & rhs ) : w(W), h(H), size(W * H) {
+    Mat2d( const Mat2d& rhs ) : w(W), h(H), size(W * H) {
         *this = rhs;
     }
-    Mat2d & operator=( const Mat2d& rhs ) {
+    Mat2d& operator=( const Mat2d& rhs ) {
         w = rhs.w;
         h = rhs.h;
         size = rhs.size;
@@ -39,7 +46,6 @@ public:
             data[i] = (!(i % (w + 1)) ? 1 : 0);
         return (*this);
     }
-
     Mat2d<T, W, H>   transpose( void ) {
         Mat2d<T, W, H> res;
         for (int i = 0; i < h; ++i)
@@ -96,7 +102,7 @@ public:
         return (res -= rhs);
     }
     template<typename _T, size_t _H, size_t _W>
-    Mat2d<_T, H,_W>   operator*( Mat2d<_T,_H,_W> & rhs ) {
+    Mat2d<_T, H,_W>   operator*( const Mat2d<_T,_H,_W>& rhs ) {
         if (rhs.h != w)
             throw Exception::MatrixOperationError(h, w, rhs.h, rhs.w);
         Mat2d<_T, H,_W>  res;
@@ -104,15 +110,12 @@ public:
             for (size_t j = 0; j < rhs.w; ++j) {
                 res(i,j) = 0;
                 for (size_t k = 0; k < w; ++k)
-                    res(i,j) += (*this)(i,k) * rhs(k,j);
+                    res(i,j) += (*this)(i,k) * rhs[k * rhs.w + j];
             }
         return (res);
     }
-    Mat2d   operator/( const Mat2d & rhs ) {
-        auto res = *this;
-        return (res /= rhs);
-    }
-    /* scalar operator overload (we could take type other than T) */
+    /*  OTHER MATRIX INSTANCE OPERATORS
+    */
     Mat2d   &operator+=( T scalar ) {
         for (size_t i = 0; i < size; ++i)
             data[i] += scalar;
@@ -133,7 +136,8 @@ public:
             data[i] /= scalar;
         return (*this);
     }
-    /* matrix operator overload */
+    /*  OTHER MATRIX INSTANCE OPERATORS
+    */
     Mat2d   &operator+=( const Mat2d& rhs ) {
         if (rhs.size != size)
             throw Exception::MatrixOperationError(size, rhs.size);
@@ -148,32 +152,10 @@ public:
             data[i] -= rhs[i];
         return (*this);
     }
-    Mat2d   &operator*=( const Mat2d& rhs ) {
-        if (rhs.h != w)
-            throw Exception::MatrixOperationError(h, w, rhs.h, rhs.w);
-        // well we would have to reshape the matrix
-        // Mat2d<T, H, W>  tmp;
-        // for (size_t i = 0; i < m; ++i) {
-        //     for (size_t j = 0; j < q; ++j) {
-        //         tmp(i,j) = 0;
-        //         for (size_t k = 0; k < n; ++k)
-        //             tmp(i,j) = c(i,j) + (a(i,k) * b(k,j));
-        //     }
-        // }
-        // *this = tmp;
-        return (*this);
-    }
-    Mat2d   &operator/=( const Mat2d& rhs ) {
-        if (rhs.h != w)
-            throw Exception::MatrixOperationError(h, w, rhs.h, rhs.w);
-        // TODO
-        return (*this);
-    }
-
-    void   compareDims( const Mat2d& rhs ) {
-        if (rhs.w != w or rhs.h != h)
-            throw Exception::MatrixError("");
-    }
+    // Mat2d   &operator*=( const Mat2d& rhs ) {
+    //     // NOTE: well we would have to reshape the matrix
+    //     return (*this);
+    // }
 
     std::array<T, W * H>    getData( void ) const { return (data); };
 
@@ -196,14 +178,81 @@ private:
 
 };
 
+/* Vectors typedefs */
 typedef Mat2d<int, 4, 1> vec4i;
 typedef Mat2d<int, 3, 1> vec3i;
 typedef Mat2d<int, 2, 1> vec2i;
 
-typedef Mat2d<float, 4, 1> vec4f;
-typedef Mat2d<float, 3, 1> vec3f;
-typedef Mat2d<float, 2, 1> vec2f;
+typedef Mat2d<float, 4, 1> vec4;
+typedef Mat2d<float, 3, 1> vec3;
+typedef Mat2d<float, 2, 1> vec2;
 
 typedef Mat2d<double, 4, 1> vec4d;
 typedef Mat2d<double, 3, 1> vec3d;
 typedef Mat2d<double, 2, 1> vec2d;
+
+/* Matrices typedefs */
+typedef Mat2d<float, 4, 4> mat4;
+typedef Mat2d<float, 3, 3> mat3;
+typedef Mat2d<float, 2, 2> mat2;
+
+namespace mtls {
+    float   radians( const float deg ) { return (deg * DEG2RAD); }
+    float   degrees( const float rad ) { return (rad * RAD2DEG); }
+
+    mat4    &scale( mat4& m, const vec3& scale ) {
+        mat4    tmp;
+        tmp(0)  = scale[0];
+        tmp(5)  = scale[1];
+        tmp(10) = scale[2];
+        m = tmp * m;
+        return (m);
+    }
+    mat4    &translate( mat4& m, const vec3& translate ) {
+        mat4    tmp;
+        tmp(3)  = translate[0];
+        tmp(7)  = translate[1];
+        tmp(11) = translate[2];
+        m = tmp * m;
+        return (m);
+    }
+    mat4    &rotate( mat4& m, double theta, const vec3& r ) {
+        const float sin = std::sin(theta);
+        const float cos = std::cos(theta);
+        mat4    tmp({
+            cos+r[0]*r[0]*(1-cos),      r[0]*r[1]*(1-cos)-r[2]*sin, r[0]*r[2]*(1-cos)+r[1]*sin, 0,
+            r[1]*r[0]*(1-cos)+r[2]*sin, cos+r[1]*r[1]*(1-cos),      r[1]*r[2]*(1-cos)-r[0]*sin, 0,
+            r[2]*r[0]*(1-cos)-r[1]*sin, r[2]*r[1]*(1-cos)+r[0]*sin, cos+r[2]*r[2]*(1-cos),      0,
+            0,                          0,                          0,                          1
+        });
+        m = tmp * m;
+        return (m);
+    }
+
+}
+
+/*
+    _IDENTITY______
+    [[ 1  0  0  0 ]
+     [ 0  1  0  0 ]
+     [ 0  0  1  0 ]
+     [ 0  0  0  1 ]]
+
+    _SCALE_________
+    [[ Sx 0  0  0 ]
+     [ 0  Sy 0  0 ]
+     [ 0  0  Sz 0 ]
+     [ 0  0  0  1 ]]
+
+    _TRANSLATE_____
+    [[ 1  0  0  Tx]
+     [ 0  1  0  Ty]
+     [ 0  0  1  Tz]
+     [ 0  0  0  1 ]]
+
+     when doing matrix multiplication it is advised to do it in the following order:
+        scaling -> rotation -> translation
+     as matrix multiplications are read from right to left, we do:
+        translation * rotation * scaling * V
+
+*/
--- src/main.cpp
+++ src/main.cpp
@@ -35,16 +35,46 @@ int main( void ) {
     auto F_T = F.transpose();
     std::cout << F_T << std::endl << std::endl;
 
-    vec4f    v4({ 4, 5, 6, 1 });
-    std::cout << v4 << std::endl;
+    vec4    v({ 4, 5, 6, 1 });
+    std::cout << v << std::endl;
 
     Mat2d<float, 4, 4>  G;
     G.identity();
     G *= 2;
+    std::cout << G * v << std::endl;
 
-    std::cout << G * v4 << std::endl;
+    /* MATRIX MULTIPLICATION IS NOT COMMUTATIVE */
+    Mat2d<float, 4, 4>  scale({
+        6, 0, 0, 0,
+        0, 6, 0, 0,
+        0, 0, 6, 0,
+        0, 0, 0, 1
+    });
+    Mat2d<float, 4, 4>  translate({
+        1, 0, 0, 1,
+        0, 1, 0, 2,
+        0, 0, 1, 3,
+        0, 0, 0, 1
+    });
+    std::cout << scale * translate << std::endl; // those two are different
+    std::cout << translate * scale << std::endl; // the order is important
+
+    std::cout << translate * scale * vec4({0.5, 0.1, 0.4, 1}) << std::endl; // should be [[4], [2.6], [5.4], [1]]
+
+
+    vec4    test({1, 0, 0, 1});
+    mat4    trans;
+
+    trans = mtls::translate(trans, vec3({ 1, 1, 0 }));
+    trans = mtls::rotate(trans, mtls::radians(90), vec3({ 0, 0, 1 }));
+    trans = mtls::scale(trans, vec3({ 0.5, 0.5, 0.5 }));
+
+    std::cout << trans * test << std::endl;
+
+    // matrix::rotate(vec4({0.5, 0.1, 0.4, 1}), matrix::radians(90), vec3());
 
     std::exit(1);
+    //===========================================
 
     Env         environment;
     Renderer    renderer(&environment);
